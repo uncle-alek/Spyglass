c6eccd442383a1d533d8c6b7b2f136604c02d33c
Aleksey Yakimenko <aleksey.yakimenko@agoda.com>
2022-07-31 14:02:59 +0700

    Add pretty JSON

--- TAGALens/TAGAAction.swift
+++ TAGALens/TAGAAction.swift
@@ -12,7 +12,9 @@ struct TAGAAction: Decodable {
     let timestamp: TimeInterval
     let payload: String
     let stateBefore: [String: Any]
+    let formattedStateBefore: String
     let stateAfter: [String: Any]
+    let formattedStateAfter: String
     
     enum CodingKeys: String, CodingKey {
         case name
@@ -28,9 +30,11 @@ struct TAGAAction: Decodable {
         timestamp = try values.decode(TimeInterval.self, forKey: .timestamp)
         payload = try values.decode(String.self, forKey: .payload)
         
-        let stateBeforeString = try values.decode(String.self, forKey: .stateBefore)
-        stateBefore = try JSONSerialization.jsonObject(with: Data(stateBeforeString.utf8), options: []) as! [String : Any]
-        let stateAfterString = try values.decode(String.self, forKey: .stateAfter)
-        stateAfter = try JSONSerialization.jsonObject(with: Data(stateAfterString.utf8), options: []) as! [String : Any]
+        let _stateBeforeString = try values.decode(String.self, forKey: .stateBefore)
+        stateBefore = try JSONSerialization.jsonObject(with: Data(_stateBeforeString.utf8), options: []) as! [String : Any]
+        formattedStateBefore = String(decoding: try JSONSerialization.data(withJSONObject: stateBefore, options: [.prettyPrinted]), as: UTF8.self)
+        let _stateAfterString = try values.decode(String.self, forKey: .stateAfter)
+        stateAfter = try JSONSerialization.jsonObject(with: Data(_stateAfterString.utf8), options: []) as! [String : Any]
+        formattedStateAfter = String(decoding: try JSONSerialization.data(withJSONObject: stateAfter, options: [.prettyPrinted]), as: UTF8.self)
     }
 }
