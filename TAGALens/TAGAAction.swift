fd1cb69be79c64dd64b6f852bb95af52f9517ff0
Aleksey Yakimenko <aleksey.yakimenko@agoda.com>
2022-08-15 17:55:41 +0700

    Remove payload

--- TAGALens/TAGAAction.swift
+++ TAGALens/TAGAAction.swift
@@ -10,7 +10,6 @@ import Foundation
 struct TAGAAction: Decodable {
     let name: String
     let timestamp: TimeInterval
-    let payload: String
     let stateBefore: [String: Any]
     let stateAfter: [String: Any]
     let file: String?
@@ -19,7 +18,6 @@ struct TAGAAction: Decodable {
     enum CodingKeys: String, CodingKey {
         case name
         case timestamp
-        case payload
         case stateBefore
         case stateAfter
         case file
@@ -30,7 +28,6 @@ struct TAGAAction: Decodable {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         name = try values.decode(String.self, forKey: .name)
         timestamp = try values.decode(TimeInterval.self, forKey: .timestamp)
-        payload = try values.decode(String.self, forKey: .payload)
         file = try values.decodeIfPresent(String.self, forKey: .file)
         line = try values.decodeIfPresent(UInt.self, forKey: .line)
         
